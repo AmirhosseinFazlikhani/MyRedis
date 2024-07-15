0e10435b2f58d990f35969a5cd9f81451b7e1028
amirhossein fazlikhani <afazlikhani98@gmail.com>
2024-07-15 19:03:30 +0330

    ping pong

---
+++ Redis.CLI/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Redis.CLI;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var port = 5000;
+        var host = "127.0.0.1";
+        
+        var argCursor = 0;
+
+        while (argCursor < args.Length)
+        {
+            switch (args[argCursor])
+            {
+                case "-p":
+                    argCursor++;
+                    port = int.Parse(args[argCursor]);
+                    break;
+                case "-h":
+                    argCursor++;
+                    host = args[argCursor];
+                    break;
+                default: 
+                    Console.WriteLine("Undefined argument: {0}", args[argCursor]);
+                    return;
+            }
+        }
+
+        var client = new TcpClient(host, port);
+        var stream = client.GetStream();
+
+        while (true)
+        {
+            Console.Write("> ");
+            var command = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(command))
+            {
+                continue;
+            }
+
+            await stream.WriteAsync(Encoding.UTF8.GetBytes(command));
+            var response = await GetResponseAsync(stream);
+            Console.WriteLine(response);
+        }
+    }
+
+    private static async Task<string> GetResponseAsync(NetworkStream stream)
+    {
+        var bytes = new byte[256];
+        int readBytesCount;
+        var response = "";
+
+        while ((readBytesCount = await stream.ReadAsync(bytes)) != 0)
+        {
+            var responseChunk = Encoding.UTF8.GetString(bytes, 0, readBytesCount);
+            
+            if (responseChunk.EndsWith("::"))
+            {
+                response += responseChunk[..^2];
+                break;
+            }
+
+            response += responseChunk;
+        }
+        
+        return response;
+    }
+}
---
+++ Redis.Server/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Redis.Server;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var port = args.Length > 1 && args[0] == "-p" ? int.Parse(args[1]) : 5000;
+        
+        TcpListener? server = null;
+
+        try
+        {
+            var localAddr = IPAddress.Parse("127.0.0.1");
+            server = new TcpListener(localAddr, port);
+            server.Start();
+
+            while (true)
+            {
+                Console.Write("Waiting for a connection... ");
+
+                using var client = await server.AcceptTcpClientAsync();
+                Console.WriteLine("Connected!");
+
+                var networkStream = client.GetStream();
+
+                var bytes = new byte[256];
+                int readBytesCount;
+
+                while ((readBytesCount = networkStream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    var command = Encoding.UTF8.GetString(bytes, 0, readBytesCount);
+
+                    if (command.Equals("ping", StringComparison.OrdinalIgnoreCase))
+                    {
+                        await networkStream.WriteAsync("PONG::"u8.ToArray());
+                    }
+                    else
+                    {
+                        networkStream.Write("Invalid command::"u8.ToArray());
+                    }
+                }
+            }
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+        finally
+        {
+            server?.Stop();
+        }
+
+        Console.WriteLine("\nHit enter to continue...");
+        Console.Read();
+    }
+}
