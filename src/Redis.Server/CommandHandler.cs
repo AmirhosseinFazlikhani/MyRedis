06a4df2cb613628f04789045c286882f0a06ae3e
amirhossein fazlikhani <afazlikhani98@gmail.com>
2024-07-17 22:26:55 +0330

    feat: Respond to HELLO command

--- src/Redis.Server/CommandHandler.cs
+++ src/Redis.Server/CommandHandler.cs
@@ -4,13 +4,41 @@ namespace Redis.Server;
 
 public static class CommandHandler
 {
-    public static Task<string> HandleAsync(string[] parameters)
+    private const int ProtoVersion = 2;
+
+    public static string Handle(string[] parameters, int connectionId)
     {
-        foreach (var parameter in parameters)
+        if (parameters[0].Equals("ping", StringComparison.OrdinalIgnoreCase))
         {
-            Console.WriteLine(parameter);
+            var reply = parameters.Length == 1 ? "PONG" : parameters[1];
+            return Serializer.SerializeSimpleString(reply);
+        }
+        
+        if (parameters[0].Equals("hello", StringComparison.OrdinalIgnoreCase))
+        {
+            if (parameters.Length == 2 && parameters[1] != ProtoVersion.ToString())
+            {
+                return Serializer.SerializeSimpleError("NOPROTO sorry, this protocol version is not supported.");
+            }
+            
+            return Serializer.SerializeArray([
+                Serializer.SerializeSimpleString("server"),
+                Serializer.SerializeSimpleString("redis"),
+                Serializer.SerializeSimpleString("version"),
+                Serializer.SerializeSimpleString("7.0.0"),
+                Serializer.SerializeSimpleString("proto"),
+                Serializer.SerializeInteger(ProtoVersion),
+                Serializer.SerializeSimpleString("id"),
+                Serializer.SerializeInteger(connectionId),
+                Serializer.SerializeSimpleString("mode"),
+                Serializer.SerializeSimpleString("standalone"),
+                Serializer.SerializeSimpleString("role"),
+                Serializer.SerializeSimpleString("master"),
+                Serializer.SerializeSimpleString("modules"),
+                Serializer.SerializeArray([]),
+            ]);
         }
 
-        return Task.FromResult(Serializer.SerializeSimpleString("OK"));
+        return Serializer.SerializeSimpleError($"ERR unknown command '{parameters[0]}'");
     }
 }
